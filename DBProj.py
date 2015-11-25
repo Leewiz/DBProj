82e61643e181bf877a1ab26835b40c44f29bd1ce
Leewiz <greeneggsnham59@gmail.com>
2015-11-24 19:11:56 -0600

    test

--- DBProj.py
+++ DBProj.py
@@ -1,2 +1,8 @@
 __author__ = 'Leewiz'
-print("hello")
+import sys
+import sqlite3
+import flask
+from contextlib import contextmanager
+
+app = flask.Flask(__name__)
+app.config.from_pyfile('settings.py')
