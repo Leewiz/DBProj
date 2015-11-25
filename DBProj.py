0bfdfd770883b1f3f24a3ff2ba307c5a7faf8f09
Travis <tls2@txstate.edu>
2015-11-24 19:09:32 -0600

    fdsa

--- DBProj.py
+++ DBProj.py
@@ -1,2 +1,15 @@
-__author__ = 'Leewiz'
-print("hello")
+import sqlite3 as lite
+import flask
+app = flask.Flask(__name__)
+import jinja2
+engine = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
+
+@app.route('/', methods=['GET'])
+def login():
+    userInfo = flask.request.form
+    print(userInfo);
+    return engine.get_template('login.html').render()
+
+if __name__ == '__main__':
+    app.run()
